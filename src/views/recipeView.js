e5acd9111d93b7f49982853d2365d11de72a28b7
syadav <suresh23_himcs@rediffmail.com>
2025-01-24 12:53:38 +0530

    Using fracty module

--- src/views/recipeView.js
+++ src/views/recipeView.js
@@ -1,6 +1,7 @@
 
 import icons from 'url:../img/icons.svg'
 import { Fraction } from 'fractional'
+import fracty from 'fracty'
 import View from './view.js'
 
 class RecipeView extends View {
@@ -115,7 +116,7 @@ class RecipeView extends View {
               <svg class="recipe__icon">
                 <use href="${icons}#icon-check"></use>
               </svg>
-              <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+              <div class="recipe__quantity">${fracty(ing.quantity) ? fracty(ing.quantity) : ''}</div>
               <div class="recipe__description">
                 <span class="recipe__unit">${ing.unit}</span>
                 ${ing.description}
